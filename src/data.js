d6c8c38efa700feb0ce571212cdb7103dc098269
susy zapari <susy.zapari@gmail.com>
2019-02-21 08:39:49 -0600

    inputs y boton ingresar

--- src/data.js
+++ src/data.js
@@ -4,5 +4,18 @@
 const example = () => {
   return 'example';
 };
-
+function fillElements (pokemonList, divElement){
+  for(let i=0; i<pokemonList.length; i++) {
+    let divPokemon = document.createElement("div");
+    divPokemon.className = "pokemon";
+    divPokemon.innerHTML = "<img src=\"" +pokemonList[i].img + "\">";
+    divElement.insertAdjacentElement("beforeend", divPokemon);
+  }
+  }
+  
+  function showPokemons(){
+  let pokemonList = POKEMON.pokemon;
+  let divPokemonList = document.getElementById("listOfPokemons");
+  fillElements(pokemonList, divPokemonList);
+  }
 window.example = example;
