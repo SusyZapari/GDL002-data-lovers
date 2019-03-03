22ac6ba6318726c706d577f8f0cb275d51224eab
marisolcastrolopez <46985112+marisolcastrolopez@users.noreply.github.com>
2019-03-03 16:58:57 -0600

    Función para regresar un nuevo arreglo con el año y la cantidad de heridos [cantidad de heridos, año]. Enlazado de páginas html para flujo de página. Falta homogenizar CSS

--- src/data.js
+++ src/data.js
@@ -4,7 +4,85 @@
   return 'example';
 };*/
 //Esta función es útil para delimitar los años del 2010 a 2015 únicamente//
+window.roadEye = {
+  
+  filtersYears:  (dataInjuries) => { 
+    const result = dataInjuries.filter(
+      injurie => {
+        return parseInt(injurie.Year.substring(0, 4)) >= 2010 && parseInt(injurie.Year.substring(0, 4)) < 2016;
+      }
+    );
+    return result;
+  },
+  
+  newArrBiciYear: (dataFiltered) => {
+    const arrMapped2 = dataFiltered.map(
+        element => {
+          return [element.Total_Injured_Persons_Pedalcyclists, parseInt(element.Year.substring(0, 4))];
+        }
+      );
+    return arrMapped2;  
+  }
 
+};
+/*
+mappedBici: (dataFiltered) => {
+  let newArr =[];
+  const arrMapped = dataFiltered.map(
+      element => {
+        return [[element.Total_Injured_Persons_Pedalcyclists],[parseInt(element.Year.substring(0, 4))]];
+      }
+    );
+  newArr = arrMapped;
+  return newArr;  
+},
+
+mappedBiciSort: (mappedBici) => {
+
+}
+*/
+
+
+
+/*mappedBici: (dataFiltered) => {
+  const arrMapped = dataFiltered.map(
+      element => {
+        return element.Total_Injured_Persons_Pedalcyclists;
+      }
+    );
+  const yearMapped = dataFiltered.map(
+      element => {
+        return parseInt(element.Year.substring(0, 4));
+      }
+    );
+    return [yearMapped, arrMapped];
+    
+},*/
+/*
+mappedBiciSort: (mappedBici) => {
+  
+}
+*/
+
+
+
+
+
+
+/*
+newArrFiltered.map(element => {
+      let xyz = element.Total_Injured_Persons_Pedalcyclists
+      ; return xyz; });
+
+
+      
+let iterador = () => {
+  let newt = [];
+    for(var i = 0; i < newArrFiltered.length; i++) {
+      let newt = newArrFiltered[i].Total_Injured_Persons_Pedalcyclists;
+    }
+    return newt;
+ };
 const filterObj = {
   filtersYears:  (dataInjuries) => { 
     const result = dataInjuries.filter(
@@ -12,9 +90,21 @@ const filterObj = {
         return parseInt(injurie.Year.substring(0, 4)) >= 2010 && parseInt(injurie.Year.substring(0, 4)) < 2016;
       }
     );
-  return result;
+    const demo = result.map(element => {
+      let xyz = element.Total_Injured_Persons_Pedalcyclists
+      ; return xyz;
+     });
+  return demo;
   }
 };
+
+const sortObj = {
+  sortYears: (data) => {
+    console.log(data);
+  }
+};
+console.log(filterObj.filtersYears);
+window.sortObj = sortObj;
 window.filterObj = filterObj;
 /*const filterYears = () => {
   const result = INJURIES.filter(
--- src/main.js
+++ src/main.js
@@ -1,11 +1,52 @@
-/*function displayInjurieData (){
-    let injurieListElement = document.querySelector ("#injuries");
+
+
+const newArrFiltered = window.roadEye.filtersYears(INJURIES);
+console.log(newArrFiltered);
+let newArr = window.roadEye.newArrBiciYear(newArrFiltered);
+console.log(newArr);
+
+/*document.getElementById("showData").addEventListener("click", displayInjuryData = () => {
+    document.getElementById("fixed-tab-2").innerHTML =  mappedData;
+    event.preventDefault();
+}, false);
+*/
+
+
+
+/*let mappedInjures = roadEye.mappedInjured(newArrFiltered);
+console.table(mappedInjures);
+let mappedYears = roadEye.mappedYear(newArrFiltered);
+console.table(mappedYears);*/
+
+/*let displayInjurieData = () => {
+    let injurieListElement = document.querySelector("#showData2");
     const injurieElement = document.createElement ("li");
     injurieElement.text = "2015";
-    injurieListElement.appendChild (injurieElement);
-console.log ("display injurie data");
+    injurieListElement.appendChild(injurieElement);
+console.log (injurieElement.text);
 }
-document.getElementById("datosCronologicosTab").addEventListener ("click", displayInjuryData);
+console.log (displayInjurieData());
+document.getElementById("showData2").addEventListener ("click", displayInjuryData);
 
-//Test
-*/
+//Display data
+/*let displayInjuryData2 = (INJURIES) => {
+var para = document.createElement("div");
+var node = document.createTextNode("Hello");
+para.appendChild(node);
+var element = document.getElementById("fixed-tab-1");
+element.appendChild(para);
+};*/
+/*let displayInjuryData = () => {
+    let injurieListElement = document.querySelector("#showData");
+    injurieListElement.onclick = () => {
+        document.getElementById("fixed-tab-1").innerHTML = filterObj.filtersYears(INJURIES);
+      };
+};*/
+/*document.getElementById("showData").addEventListener("click", displayInjuryData);*/
+//Event Listeners
+/*document.getElementById("showData").addEventListener("click", displayInjuryData = () => {
+    document.getElementById("fixed-tab-1").innerHTML = filterObj.filtersYears(INJURIES)
+    event.preventDefault();
+    console.log(displayInjuryData);
+}, false);*/
+
