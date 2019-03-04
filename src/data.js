55fd64fd6287e6b63f8817fef6f4438223dedf0e
marisolcastrolopez <46985112+marisolcastrolopez@users.noreply.github.com>
2019-03-04 02:52:06 -0600

    Sort funciona

--- src/data.js
+++ src/data.js
@@ -1,44 +1,65 @@
+window.roadEye = {
+
+  filtersYears: (dataInjuries) => {
+    const result = dataInjuries.filter(
+      injurie => {
+        return parseInt(injurie.Year.substring(0, 4)) >= 2010 && parseInt(injurie.Year.substring(0, 4)) < 2016;
+      }
+    );
+    return result;
+  },
+
+  newArrBiciYear: (dataFiltered) => {
+    let order = 0;
+    const arrMapped2 = dataFiltered.map(
+      element => {
+        return [++order, parseInt(element.Year.substring(0, 4)), element.Total_Injured_Persons_Pedalcyclists];
+      }
+    );
+    return arrMapped2;
+  },
+
+  sortYearsOld: (dataOldtoNew) => {
+    let result = dataOldtoNew.sort(
+      function (a, b) { return a[1] - b[1] }
+    );
+    return result;
+  },
+
+  sortYearsNew: (dataNewtoOld) => {
+    const result2 = dataNewtoOld.sort(
+      function (a, b) { return b[1] - a[1] }
+    );
+    return result2;
+  },
+
+  getPercent: (oldNumber, newNumber) => {
+    let decreaseValue = oldNumber - newNumber;
+
+    return Math.floor((decreaseValue / oldNumber) * 100);
+},
+
+  fillTableBici: (table, data) => {
+    for (var i = 0; i < data.length; i++) {
+      // create a new row
+      var newRow = table.insertRow(table.length);
+      for (var j = 0; j < data[i].length; j++) {
+        // create a new cell
+        var cell = newRow.insertCell(j);
+        // add value to the cell
+        cell.innerHTML = data[i][j];
+      }
+    }
+  }
+
+};
+
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 /*const example = () => {
   return 'example';
 };*/
 //Esta función es útil para delimitar los años del 2010 a 2015 únicamente//
-window.roadEye = {
-
-    filtersYears: (dataInjuries) => {
-        const result = dataInjuries.filter(
-            injurie => {
-                return parseInt(injurie.Year.substring(0, 4)) >= 2010 && parseInt(injurie.Year.substring(0, 4)) < 2016;
-            }
-        );
-        return result;
-    },
-
-    newArrBiciYear: (dataFiltered) => {
-        let order = 0;
-        const arrMapped2 = dataFiltered.map(
-            element => {
-                return [++order, parseInt(element.Year.substring(0, 4)), element.Total_Injured_Persons_Pedalcyclists];
-            }
-        );
-        return arrMapped2;
-    },
-
-    fillTableBici: (table, data) => {
-        for (var i = 0; i < data.length; i++) {
-            // create a new row
-            var newRow = table.insertRow(table.length);
-            for (var j = 0; j < data[i].length; j++) {
-                // create a new cell
-                var cell = newRow.insertCell(j);
-                // add value to the cell
-                cell.innerHTML = data[i][j];
-            }
-        }
-    }
-
-};
 /*
 mappedBici: (dataFiltered) => {
   let newArr =[];
--- src/main.js
+++ src/main.js
@@ -1,13 +1,137 @@
 
 //Print Bicycle Data in Table
-const newArrFiltered = window.roadEye.filtersYears(INJURIES);
+/*const newArrFiltered = window.roadEye.filtersYears(INJURIES);
 console.log(newArrFiltered);
 let newArr = window.roadEye.newArrBiciYear(newArrFiltered);
 console.log(newArr);
-let tableHtmlBici = document.querySelector('#tableBici tbody');
-window.roadEye.fillTableBici(tableHtmlBici, newArr);
 
-document.getElementById('tabCrypt').addEventListener('click',selectTab);
+const sortOld = window.roadEye.sortYears(newArr);
+console.log(sortOld);
+
+const sortNew = window.roadEye.other(newArr);
+console.log(sortNew);
+
+
+let tableHtmlBici = document.querySelector('#tableBici tbody');
+window.roadEye.fillTableBici(tableHtmlBici, newArr);*/
+
+/*
+let selectedBike = () => {
+    const newArrFiltered = window.roadEye.filtersYears(INJURIES);
+    let newArr = window.roadEye.newArrBiciYear(newArrFiltered);
+    newArr = window.roadEye.sortYearsOld(newArr);
+    console.log(newArr);
+
+//Loop for tabs, the user can choose between them.	
+	/*button1.onclick = function() {
+	document.getElementById("tab1").className = "visible";
+    document.getElementById("tab2").className = "invisible";
+    button1.className = "activated";
+    button2.className = "deactivated";
+  };
+	button2.onclick = function() {
+    document.getElementById("tab1").className = "invisible";
+    document.getElementById("tab2").className = "visible";
+    button1.className = "deactivated";
+    button2.className = "activated";
+  };*/
+//};
+const newArrFiltered = window.roadEye.filtersYears(INJURIES);
+let newArr = window.roadEye.newArrBiciYear(newArrFiltered);
+newArr = window.roadEye.sortYearsOld(newArr);
+let tableHtmlBici = document.querySelector('#tableBici tbody');
+var mine = window.roadEye.fillTableBici(tableHtmlBici, newArr);
+
+let oldnew = () => {
+    const buttonOld = document.querySelector('#option-one-old');
+
+    buttonOld.onclick = function () {
+
+        const newArrFiltered = window.roadEye.filtersYears(INJURIES);
+        let newArr = window.roadEye.newArrBiciYear(newArrFiltered);
+        newArr = window.roadEye.sortYearsOld(newArr);
+        let tableHtmlBici = document.querySelector('#tableBici tbody');
+        tableHtmlBici.innerHTML = "";
+        window.roadEye.fillTableBici(tableHtmlBici, newArr);
+    };
+};
+
+let newOld = () => {
+    const buttonNew = document.querySelector('#option-two-new');
+    buttonNew.onclick = function () {
+        const newArrFiltered = window.roadEye.filtersYears(INJURIES);
+        let newArr = window.roadEye.newArrBiciYear(newArrFiltered);
+        newArr = window.roadEye.sortYearsNew(newArr);
+        let tableHtmlBici = document.querySelector('#tableBici tbody');
+        tableHtmlBici.innerHTML = "";
+        window.roadEye.fillTableBici(tableHtmlBici, newArr);
+    };
+};
+
+document.getElementById('option-one-old').addEventListener('click', oldnew)
+document.getElementById('option-two-new').addEventListener('click', newOld)
+
+/*
+//Event Listeners
+document.getElementById('option-one-old').addEventListener('click', selectedBike = () => {
+    newArr = window.roadEye.sortYearsOld(newArr);
+    let tableHtmlBici = document.querySelector('#tableBici tbody');
+    window.roadEye.fillTableBici(tableHtmlBici, newArr);
+    return selectedBike;
+
+});
+document.getElementById('option-two-new').addEventListener('click', selectedBike2 = () => {
+    newArr = window.roadEye.sortYearsNew(newArr);
+    let tableHtmlBici = document.querySelector('#tableBici tbody');
+    window.roadEye.fillTableBici(tableHtmlBici, newArr);
+    return selectedBike2;
+})
+
+
+
+
+
+
+
+
+
+//Event Listeners      event.preventDefault();
+/*document.getElementById('button-choose-bike').addEventListener('click',selectedBike = () => {
+    const button1 = document.querySelector('#button-choose-bike');
+
+    console.log(newArr);
+
+})*/
+
+
+
+
+
+
+/*
+document.getElementById('button-choose-bike').addEventListener('click',selectedBike = () => {
+    const button1 = document.querySelector('#button-choose-bike');
+    const newArrFiltered = window.roadEye.filtersYears(INJURIES);
+    let newArr = window.roadEye.newArrBiciYear(newArrFiltered);
+    newArr = window.roadEye.sortYearsOld(newArr);
+    console.log(newArr);
+
+    let tableHtmlBici = document.querySelector('#tableBici tbody');
+window.roadEye.fillTableBici(tableHtmlBici, newArr);
+});*/
+
+
+//Event Listeners
+/*document.getElementById('tabCrypt').addEventListener('click',selectTab);
+document.getElementById('tabUncrypt').addEventListener('click',selectTab);*/
+
+
+
+/*const sort = window.roadEye.sortYearsOldtoNew(newArr);
+console.log(sort);
+const sort2 = window.roadEye.sortYearsNewtoOld(newArr);
+console.log(sort2);*/
+
 
 /*[[52000, 2010, 1],[48000, 2011, 2],[49000, 2012, 3],[48000, 2013, 4],[50000, 2014, 5],[45000, 2015, 6]]*/
 
@@ -53,6 +177,7 @@ element.appendChild(para);
 /*document.getElementById("showData").addEventListener("click", displayInjuryData = () => {
     document.getElementById("fixed-tab-1").innerHTML = filterObj.filtersYears(INJURIES)
     event.preventDefault();
-    console.log(displayInjuryData);
-}, false);*/
+//    console.log(displayInjuryData);
+//}, false);
+*/
 
